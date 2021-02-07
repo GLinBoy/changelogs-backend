357708e3c7e65e156df38b45ae7c1be4cd8e54f3
GLinBoy <hojjatabedie@gmail.com>
2021-02-07 21:38:56 +0330

    📦 NEW: Added Base & Auditable entities

---
+++ src/main/java/info/changelogs/app/entity/Auditable.java
@@ -0,0 +1,45 @@
+package info.changelogs.app.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(callSuper = false)
+public abstract class Auditable extends BaseEntity {
+
+	@CreatedBy
+	@Column(name = "CREATED_BY", updatable = false, nullable = false)
+	private Long createdBy;
+
+	@LastModifiedBy
+	@Column(name = "EDITED_BY", nullable = false)
+	private Long editedBy;
+
+	@CreatedDate
+	@Column(name = "CREATED_ON", updatable = false, nullable = false)
+	private LocalDateTime createdOn;
+
+	@LastModifiedDate
+	@Column(name = "EDITED_ON", nullable = false)
+	private LocalDateTime editedOn;
+
+	@Version
+	@Column(name = "VERSION", nullable = false)
+	private Long version;
+
+}
---
+++ src/main/java/info/changelogs/app/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package info.changelogs.app.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+public abstract class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "ID", updatable = false, nullable = false)
+	private Long id;
+
+	@Column(name = "IS_ACTIVE", nullable = false)
+	private Boolean isActive;
+}
