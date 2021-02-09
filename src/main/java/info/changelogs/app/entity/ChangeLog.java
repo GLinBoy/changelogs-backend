bc4d9f4b25a7bda95eff85ae019c013a268b69c7
GLinBoy <hojjatabedie@gmail.com>
2021-02-09 13:17:09 +0330

    👌 IMPROVE: Update db field size of changelog entity

--- src/main/java/info/changelogs/app/entity/ChangeLog.java
+++ src/main/java/info/changelogs/app/entity/ChangeLog.java
@@ -4,7 +4,10 @@ import java.time.Instant;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
@@ -26,13 +29,27 @@ import lombok.experimental.FieldNameConstants;
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class ChangeLog extends Auditable {
+	
+	@Column(length = 32, unique = true, nullable = false)
 	private String versionNo;
+	
+	@Column(length = 32, unique = true)
 	private String buildVersion;
+	
+	@Column(nullable = false)
 	private Instant releaseDate;
+	
+	@Column(length = 128, nullable = false)
 	private String publisher;
+	
+	@Column(length = 128, nullable = false)
 	private String contact;
+	
 	private Boolean forceUpdate;
-	private String platform;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 8, nullable = false)
+	private Platform platform;
 	
 	@ManyToOne
 	@JoinColumn(name = "PROJECT_ID", nullable = false, foreignKey = @ForeignKey(name = "FK_PROJECT_CHANGELOG"))
