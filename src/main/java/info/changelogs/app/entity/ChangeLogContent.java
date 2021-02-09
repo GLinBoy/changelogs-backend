b7dbc154858ae46b04eb32c7b77145445186d819
GLinBoy <hojjatabedie@gmail.com>
2021-02-09 13:24:28 +0330

    👌 IMPROVE: Update db field size changelog content

--- src/main/java/info/changelogs/app/entity/ChangeLogContent.java
+++ src/main/java/info/changelogs/app/entity/ChangeLogContent.java
@@ -1,6 +1,9 @@
 package info.changelogs.app.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +20,11 @@ import lombok.experimental.FieldNameConstants;
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class ChangeLogContent extends Auditable {
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 10, nullable = false)
 	private ContentType contentType;
+	
+	@Column(columnDefinition = "TEXT", nullable = false)
 	private String content;
 }
