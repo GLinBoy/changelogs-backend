5ceedf1674b8d887ce6523424ce5f7e8c212d567
GLinBoy <hojjatabedie@gmail.com>
2021-03-29 16:23:43 +0430

    👌 IMPROVE: Enhanced ProjectDTO
    resolved #23
    fixed #24

--- src/main/java/info/changelogs/app/dto/ProjectDTO.java
+++ src/main/java/info/changelogs/app/dto/ProjectDTO.java
@@ -1,5 +1,10 @@
 package info.changelogs.app.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,15 +20,39 @@ import lombok.experimental.FieldNameConstants;
 @EqualsAndHashCode(callSuper = true)
 public class ProjectDTO extends BaseDTO{
 	
+	@Size(min = 4, max = 128)
+	@NotBlank(message = "Name is mandatory")
 	private String name;
+	
+	@Size(min = 4, max = 128)
+	@NotBlank(message = "Title is mandatory")
 	private String title;
+	
 	private String about;
+	
 	private String logo;
+	
+	@Size(max = 128)
+	@NotBlank(message = "Owner is mandatory")
 	private String owner;
+	
 	private Boolean publicAccess;
+	
+	@URL
+	@Size(max = 128)
 	private String website;
+	
+	@URL
+	@Size(max = 128)
 	private String readmeLink;
+	
+	@Size(max = 32)
+	private String license;
+	
+	@URL
+	@Size(max = 128)
 	private String licenseLink;
+	
 	private Long organizationId;
 
 }
