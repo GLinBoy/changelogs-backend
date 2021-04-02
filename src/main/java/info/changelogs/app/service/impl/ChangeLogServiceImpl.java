ac5f8cb4b77918a47fa4d37479951a236a140d7e
GLinBoy <hojjatabedie@gmail.com>
2021-04-02 21:53:48 +0430

    🐛 FIX: Save changelog

--- src/main/java/info/changelogs/app/service/impl/ChangeLogServiceImpl.java
+++ src/main/java/info/changelogs/app/service/impl/ChangeLogServiceImpl.java
@@ -1,10 +1,13 @@
 package info.changelogs.app.service.impl;
 
+import javax.persistence.EntityManager;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import info.changelogs.app.dto.ChangeLogDTO;
 import info.changelogs.app.entity.ChangeLog;
+import info.changelogs.app.entity.Project;
 import info.changelogs.app.repository.ChangeLogRepository;
 import info.changelogs.app.service.ChangeLogServiceApi;
 
@@ -12,9 +15,21 @@ import info.changelogs.app.service.ChangeLogServiceApi;
 public class ChangeLogServiceImpl
 	extends GenericServiceImpl<ChangeLogDTO, ChangeLog, ChangeLogRepository>
 	implements ChangeLogServiceApi {
+	
+	private final EntityManager em;
 
-	public ChangeLogServiceImpl(ChangeLogRepository repository, ModelMapper mapper) {
+	public ChangeLogServiceImpl(ChangeLogRepository repository, ModelMapper mapper, EntityManager em) {
 		super(repository, mapper, ChangeLogDTO.class, ChangeLog.class);
+		this.em = em;
 	}
-
+	
+	@Override
+	public ChangeLogDTO save(ChangeLogDTO changeLogDTO) {
+		// FIXME Check project belong to current user or has access to it
+		// https://github.com/GLinBoy/changelogs-backend/issues/28
+		ChangeLog changeLog = mapper.map(changeLogDTO, ChangeLog.class);
+		changeLog.setIsActive(Boolean.TRUE);
+		changeLog.setProject(em.getReference(Project.class, changeLogDTO.getProjectId()));
+		return mapper.map(repository.save(changeLog), ChangeLogDTO.class);
+	}
 }
