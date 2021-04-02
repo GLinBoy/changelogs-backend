091b06d38c53b323d50de1883fd918a802c9e902
GLinBoy <hojjatabedie@gmail.com>
2021-04-02 21:33:18 +0430

    🐛 FIX: serialize/deserialize Date and TIme

--- src/main/java/info/changelogs/app/ChangeLogsApplication.java
+++ src/main/java/info/changelogs/app/ChangeLogsApplication.java
@@ -1,10 +1,16 @@
 package info.changelogs.app;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import info.changelogs.app.util.ApplicationInformation;
 import info.changelogs.app.util.ApplicationProperties;
@@ -14,8 +20,15 @@ import info.changelogs.app.util.ApplicationProperties;
 @EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
 public class ChangeLogsApplication {
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChangeLogsApplication.class, args);
 	}
 
+	@PostConstruct
+	public void setUp() {
+		objectMapper.registerModule(new JavaTimeModule());
+	}
 }
