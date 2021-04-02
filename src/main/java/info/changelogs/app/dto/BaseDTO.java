b2708d081035bebce62284e13a7012e7c890c98b
GLinBoy <hojjatabedie@gmail.com>
2021-04-02 21:36:38 +0430

    👌 IMPROVE: Ignore unknown fields

--- src/main/java/info/changelogs/app/dto/BaseDTO.java
+++ src/main/java/info/changelogs/app/dto/BaseDTO.java
@@ -1,8 +1,11 @@
 package info.changelogs.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class BaseDTO {
 
 	private Long id;
