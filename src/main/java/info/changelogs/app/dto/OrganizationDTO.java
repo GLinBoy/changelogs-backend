6ba46256005baee0657632e0cf03c62b05188ce0
GLinBoy <hojjatabedie@gmail.com>
2021-04-05 23:14:19 +0430

    👌 IMPROVE: Added additional fields to OrganizationDTO
    resolved #14

--- src/main/java/info/changelogs/app/dto/OrganizationDTO.java
+++ src/main/java/info/changelogs/app/dto/OrganizationDTO.java
@@ -4,6 +4,8 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.URL;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,10 +28,24 @@ public class OrganizationDTO extends BaseDTO {
 	@Size(min = 4, max = 32)
 	@NotBlank(message = "Title is mandatory")
 	private String title;
+	
+	@Size(max = 256)
+	private String slogan;
+	
+	@URL
+	@Size(max = 64)
+	private String website;
 
 	@Email(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$")
 	@Size(min = 8, max = 64)
 	@NotBlank(message = "Email is mandatory")
 	private String email;
+	
+	@Size(max = 64)
+	private String location;
+	
+	private String logo;
+	
+	private Boolean verified;
 
 }
