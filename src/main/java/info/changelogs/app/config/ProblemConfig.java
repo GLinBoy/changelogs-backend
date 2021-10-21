21d5bac212cc2642adc1ccdb42aa545240e8ff27
GLinBoy <hojjatabedie@gmail.com>
2021-10-21 20:32:48 +0330

    🐛 FIX: Updated import

--- src/main/java/info/changelogs/app/config/ProblemConfig.java
+++ src/main/java/info/changelogs/app/config/ProblemConfig.java
@@ -2,7 +2,7 @@ package info.changelogs.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.problem.ProblemModule;
+import org.zalando.problem.jackson.ProblemModule;
 import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
