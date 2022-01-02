c33e89e34369eba5bc818c03e14af32b7d7667c7
GLinBoy <hojjatabedie@gmail.com>
2022-01-02 22:16:58 +0330

    📦 NEW: Add test structure for ChangeLogRepository
    #92

---
+++ src/test/java/info/changelogs/app/repository/ChangeLogRepositoryUnitTest.java
@@ -0,0 +1,34 @@
+package info.changelogs.app.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChangeLogRepositoryUnitTest {
+
+	@Test
+	void testFindAllDetailed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAllByUsernameAndProjectTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAllByUsernameAndProjectTitleAndVersion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAllByProjectTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAllByProjectTitleAndVersion() {
+		fail("Not yet implemented");
+	}
+
+}
