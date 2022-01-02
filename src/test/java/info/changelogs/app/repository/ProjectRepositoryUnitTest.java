e038100259c52de7fc47a5787c0bd4f13d39f9c8
GLinBoy <hojjatabedie@gmail.com>
2022-01-02 22:31:21 +0330

    📦 NEW: Added ProjectRepository test structure

---
+++ src/test/java/info/changelogs/app/repository/ProjectRepositoryUnitTest.java
@@ -0,0 +1,19 @@
+package info.changelogs.app.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectRepositoryUnitTest {
+
+	@Test
+	void testFindAllNecessary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindOneByTitle() {
+		fail("Not yet implemented");
+	}
+
+}
