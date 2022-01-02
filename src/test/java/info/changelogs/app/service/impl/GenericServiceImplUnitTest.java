fe30c2d16a6b2871504ffc6671cdfb986af96886
GLinBoy <hojjatabedie@gmail.com>
2022-01-02 22:33:23 +0330

    📦 NEW: Added GenericServiceImpl test structure

---
+++ src/test/java/info/changelogs/app/service/impl/GenericServiceImplUnitTest.java
@@ -0,0 +1,54 @@
+package info.changelogs.app.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GenericServiceImplUnitTest {
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSaveAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSingleById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteSingleById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteSingleByReference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteAll() {
+		fail("Not yet implemented");
+	}
+
+}
