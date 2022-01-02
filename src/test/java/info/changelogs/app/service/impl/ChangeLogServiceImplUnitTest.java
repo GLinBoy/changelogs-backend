93915294b8d88c2e4dd08760551479a92a8082c9
GLinBoy <hojjatabedie@gmail.com>
2022-01-02 22:34:26 +0330

    📦 NEW: Added ChangeLogServiceImpl test structure
    #99

---
+++ src/test/java/info/changelogs/app/service/impl/ChangeLogServiceImplUnitTest.java
@@ -0,0 +1,44 @@
+package info.changelogs.app.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChangeLogServiceImplUnitTest {
+
+	@Test
+	void testChangeLogServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSaveChangeLogDTO() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLatest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjectChangeLogStringStringPageable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjectChangeLogStringPageable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjectChangeLogVersionStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjectChangeLogVersionStringString() {
+		fail("Not yet implemented");
+	}
+
+}
