42c8d6916cc7cb1baf7d2e9e387672cac7fc20e4
GLinBoy <hojjatabedie@gmail.com>
2022-01-02 22:35:28 +0330

    📦 NEW: Added OrganizationServiceImpl test structure
    #100

---
+++ src/test/java/info/changelogs/app/service/impl/OrganizationServiceImplUnitTest.java
@@ -0,0 +1,19 @@
+package info.changelogs.app.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrganizationServiceImplUnitTest {
+
+	@Test
+	void testOrganizationServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetOwners() {
+		fail("Not yet implemented");
+	}
+
+}
