d7f8525d8a545dde46fdfca6ca28907912eecca8
GLinBoy <hojjatabedie@gmail.com>
2022-01-02 22:30:30 +0330

    📦 NEW: Added OrganizationRepository test structure
    #93

---
+++ src/test/java/info/changelogs/app/repository/OrganizationRepositoryUnitTest.java
@@ -0,0 +1,14 @@
+package info.changelogs.app.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrganizationRepositoryUnitTest {
+
+	@Test
+	void testFindAllByCreatedBy() {
+		fail("Not yet implemented");
+	}
+
+}
