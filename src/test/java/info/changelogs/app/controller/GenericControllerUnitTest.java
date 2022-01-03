f51e011960c761383d3d1dc7b82b35a03caf4e1b
GLinBoy <hojjatabedie@gmail.com>
2022-01-03 18:51:43 +0330

    📦 NEW: Added GenericController test structure
    #102

---
+++ src/test/java/info/changelogs/app/controller/GenericControllerUnitTest.java
@@ -0,0 +1,39 @@
+package info.changelogs.app.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GenericControllerUnitTest {
+
+	@Test
+	void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGenericController() {
+		fail("Not yet implemented");
+	}
+
+}
