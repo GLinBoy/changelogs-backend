b71f5b066049e525e12333fe49c4abdba101dd1f
GLinBoy <hojjatabedie@gmail.com>
2022-01-03 18:55:19 +0330

    📦 NEW: Added OrganizationController test structure
    #105

---
+++ src/test/java/info/changelogs/app/controller/OrganizationControllerUnitTest.java
@@ -0,0 +1,14 @@
+package info.changelogs.app.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrganizationControllerUnitTest {
+
+	@Test
+	void testGetOwnerList() {
+		fail("Not yet implemented");
+	}
+
+}
