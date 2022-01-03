3d9f64f7813be9bf0da4c741dd4a2b4b8ce6030d
GLinBoy <hojjatabedie@gmail.com>
2022-01-03 18:56:13 +0330

    Added ProjectController test structure
    #106

---
+++ src/test/java/info/changelogs/app/controller/ProjectControllerUnitTest.java
@@ -0,0 +1,19 @@
+package info.changelogs.app.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectControllerUnitTest {
+
+	@Test
+	void testGetUserProjectList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjectDetailByTitle() {
+		fail("Not yet implemented");
+	}
+
+}
