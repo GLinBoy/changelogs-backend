0c79dd98c7f6cc39f39de0eb199fbb1fafcdd7f4
GLinBoy <hojjatabedie@gmail.com>
2022-01-03 18:52:48 +0330

    📦 NEW: Added ChangeLogController test structure
    #103

---
+++ src/test/java/info/changelogs/app/controller/ChangeLogControllerUnitTest.java
@@ -0,0 +1,29 @@
+package info.changelogs.app.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChangeLogControllerUnitTest {
+
+	@Test
+	void testChangeLogController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLatest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjectChangeLog() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjectChangeLogVersion() {
+		fail("Not yet implemented");
+	}
+
+}
