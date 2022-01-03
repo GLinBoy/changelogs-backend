fdf431b060b780d3a9fc5985eeac1188c027221d
GLinBoy <hojjatabedie@gmail.com>
2022-01-03 18:50:26 +0330

    📦 NEW: Added ProjectServiceImpl test structure
    #101

---
+++ src/test/java/info/changelogs/app/service/impl/ProjectServiceImplUnitTest.java
@@ -0,0 +1,24 @@
+package info.changelogs.app.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectServiceImplUnitTest {
+
+	@Test
+	void testProjectServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllNecessary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjectDetailByTitle() {
+		fail("Not yet implemented");
+	}
+
+}
