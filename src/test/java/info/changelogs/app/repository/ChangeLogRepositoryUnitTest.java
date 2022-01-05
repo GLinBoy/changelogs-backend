c702c667e6da1fa1246ff8ba6aa8c5b03ba0a444
GLinBoy <hojjatabedie@gmail.com>
2022-01-05 19:11:24 +0330

    📦 NEW: Check ChangeLogRepository loaded in context
    #92

--- src/test/java/info/changelogs/app/repository/ChangeLogRepositoryUnitTest.java
+++ src/test/java/info/changelogs/app/repository/ChangeLogRepositoryUnitTest.java
@@ -1,10 +1,22 @@
 package info.changelogs.app.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest
 class ChangeLogRepositoryUnitTest {
+
+	@Autowired
+	private ChangeLogRepository changeLogRepository;
+
+	@Test
+	void contextLoads() {
+		assertThat(changeLogRepository).isNotNull();
+	}
 
 	@Test
 	void testFindAllDetailed() {
