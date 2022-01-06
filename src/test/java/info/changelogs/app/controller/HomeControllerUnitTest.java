49bd6f482c6c75e2ac8725504e1c53f2073e8cc6
GLinBoy <hojjatabedie@gmail.com>
2022-01-06 19:57:26 +0330

    📦 NEW: Check HomeController loaded in context
    #104

--- src/test/java/info/changelogs/app/controller/HomeControllerUnitTest.java
+++ src/test/java/info/changelogs/app/controller/HomeControllerUnitTest.java
@@ -1,11 +1,24 @@
 package info.changelogs.app.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class HomeControllerUnitTest {
 
+	@InjectMocks
+	private HomeController homeController;
+	
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(homeController).isNotNull();
+	}
+	
 	@Test
 	void testRedirect() {
 		fail("Not yet implemented");
