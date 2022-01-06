7656e1da71930eddf13191299400f7590333cc0c
GLinBoy <hojjatabedie@gmail.com>
2022-01-06 17:21:47 +0330

    NEW: Check ProjectRepository loaded in context
    #93

--- src/test/java/info/changelogs/app/repository/ProjectRepositoryUnitTest.java
+++ src/test/java/info/changelogs/app/repository/ProjectRepositoryUnitTest.java
@@ -1,10 +1,22 @@
 package info.changelogs.app.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest
 class ProjectRepositoryUnitTest {
+	
+	@Autowired
+	private ProjectRepository projectRepository;
+	
+	@Test
+	void contextLoads() {
+		assertThat(projectRepository).isNotNull();
+	}
 
 	@Test
 	void testFindAllNecessary() {
