c483fb7a75aaf28f7a6d3dc0f0aa6dbc10ccd394
GLinBoy <hojjatabedie@gmail.com>
2022-01-06 19:46:05 +0330

    📦 NEW: Check ProjectServiceImpl loaded in context
    #101

--- src/test/java/info/changelogs/app/service/impl/ProjectServiceImplUnitTest.java
+++ src/test/java/info/changelogs/app/service/impl/ProjectServiceImplUnitTest.java
@@ -1,10 +1,42 @@
 package info.changelogs.app.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.modelmapper.ModelMapper;
 
+import info.changelogs.app.repository.ProjectRepository;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class ProjectServiceImplUnitTest {
+
+	@InjectMocks
+	private ProjectServiceImpl projectService;
+
+	@Mock
+	private ProjectRepository projectRepository;
+
+	@Mock
+	private ModelMapper modelMapper;
+
+	@BeforeEach
+	void setUp() {
+		projectService = new ProjectServiceImpl(projectRepository, modelMapper);
+	}
+
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(projectService).isNotNull();
+	}
 
 	@Test
 	void testProjectServiceImpl() {
