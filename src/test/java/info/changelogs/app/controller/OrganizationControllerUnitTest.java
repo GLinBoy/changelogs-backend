05a85a048a74234fc9019dd00f7ebdcbfb2132f3
GLinBoy <hojjatabedie@gmail.com>
2022-01-06 19:59:39 +0330

    📦 NEW: Check OrganizationController loaded in context
    #105

--- src/test/java/info/changelogs/app/controller/OrganizationControllerUnitTest.java
+++ src/test/java/info/changelogs/app/controller/OrganizationControllerUnitTest.java
@@ -1,10 +1,26 @@
 package info.changelogs.app.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import info.changelogs.app.service.OrganizationServiceApi;
 
 class OrganizationControllerUnitTest {
+
+	@InjectMocks
+	private OrganizationController organizationController;
+
+	@Mock
+	private OrganizationServiceApi organizationService;
+
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(organizationController).isNotNull();
+	}
 
 	@Test
 	void testGetOwnerList() {
