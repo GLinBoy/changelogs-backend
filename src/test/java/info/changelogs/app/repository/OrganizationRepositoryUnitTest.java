bed4075635a049d2521be04d3fb1e5369533fc53
GLinBoy <hojjatabedie@gmail.com>
2022-01-06 17:21:30 +0330

    NEW: Check OrganizationRepository loaded in context
    #93

--- src/test/java/info/changelogs/app/repository/OrganizationRepositoryUnitTest.java
+++ src/test/java/info/changelogs/app/repository/OrganizationRepositoryUnitTest.java
@@ -1,10 +1,22 @@
 package info.changelogs.app.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest
 class OrganizationRepositoryUnitTest {
+	
+	@Autowired
+	private OrganizationRepository organizationRepository;
+	
+	@Test
+	void contextLoads() {
+		assertThat(organizationRepository).isNotNull();
+	}
 
 	@Test
 	void testFindAllByCreatedBy() {
