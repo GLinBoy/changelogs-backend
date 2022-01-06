e702eeec5ceb9e7267ab1eae5227d26b29ba6f60
GLinBoy <hojjatabedie@gmail.com>
2022-01-06 19:39:25 +0330

    📦 NEW: Check ChangeLogServiceImpl loaded in context
    #99

--- src/test/java/info/changelogs/app/service/impl/ChangeLogServiceImplUnitTest.java
+++ src/test/java/info/changelogs/app/service/impl/ChangeLogServiceImplUnitTest.java
@@ -1,10 +1,47 @@
 package info.changelogs.app.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.modelmapper.ModelMapper;
 
+import info.changelogs.app.repository.ChangeLogRepository;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class ChangeLogServiceImplUnitTest {
+
+	@InjectMocks
+	private ChangeLogServiceImpl changeLogService;
+
+	@Mock
+	private ChangeLogRepository changeLogRepository;
+
+	@Mock
+	private ModelMapper modelMapper;
+
+	@Mock
+	private EntityManager em;
+
+	@BeforeEach
+	void setUp() {
+		changeLogService = new ChangeLogServiceImpl(changeLogRepository, modelMapper, em);
+	}
+
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(changeLogService).isNotNull();
+	}
 
 	@Test
 	void testChangeLogServiceImpl() {
