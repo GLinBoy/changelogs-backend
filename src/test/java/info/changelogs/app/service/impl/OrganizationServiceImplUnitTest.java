ae0320000d83a68e373e638112c38939edae2d81
GLinBoy <hojjatabedie@gmail.com>
2022-01-06 19:43:28 +0330

    NEW: Check OrganizationServiceImpl loaded in context
    #100

--- src/test/java/info/changelogs/app/service/impl/OrganizationServiceImplUnitTest.java
+++ src/test/java/info/changelogs/app/service/impl/OrganizationServiceImplUnitTest.java
@@ -1,10 +1,42 @@
 package info.changelogs.app.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.modelmapper.ModelMapper;
 
+import info.changelogs.app.repository.OrganizationRepository;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class OrganizationServiceImplUnitTest {
+
+	@InjectMocks
+	private OrganizationServiceImpl organizationService;
+
+	@Mock
+	private OrganizationRepository organizationRepository;
+
+	@Mock
+	private ModelMapper modelMapper;
+
+	@BeforeEach
+	void setUp() {
+		organizationService = new OrganizationServiceImpl(organizationRepository, modelMapper);
+	}
+
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(organizationService).isNotNull();
+	}
 
 	@Test
 	void testOrganizationServiceImpl() {
