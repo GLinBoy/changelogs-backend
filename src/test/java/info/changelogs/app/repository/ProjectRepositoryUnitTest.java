3aa2cf2fdb53c0e715b4d7eb02f7fc0177dad5d5
GLinBoy <hojjatabedie@gmail.com>
2022-01-07 13:09:16 +0330

    👌 IMPROVE: Add ProjectRepository.findAllNecessary unit test
    #94

--- src/test/java/info/changelogs/app/repository/ProjectRepositoryUnitTest.java
+++ src/test/java/info/changelogs/app/repository/ProjectRepositoryUnitTest.java
@@ -1,14 +1,16 @@
 package info.changelogs.app.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
+import info.changelogs.app.dto.projection.ProjectMinimizedDTO;
 import info.changelogs.app.entity.Project;
 
 @DataJpaTest
@@ -24,7 +26,10 @@ class ProjectRepositoryUnitTest {
 
 	@Test
 	void testFindAllNecessary() {
-		fail("Not yet implemented");
+		Page<ProjectMinimizedDTO> projectsPage = projectRepository
+				.findAllNecessary("anonymouse", PageRequest.of(0, 1_000));
+		assertThat(projectsPage.getTotalElements()).isPositive();
+		assertThat(projectsPage.getTotalElements()).isEqualTo(5);
 	}
 
 	@Test
