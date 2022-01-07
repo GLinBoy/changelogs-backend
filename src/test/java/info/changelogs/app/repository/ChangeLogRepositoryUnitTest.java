a14466c44f1c1ee3604bb513caa3a40dd6d4edf0
GLinBoy <hojjatabedie@gmail.com>
2022-01-07 14:08:59 +0330

    📦 NEW: Added ChangeLogRepository.findAllDetailed unit test
    #92

--- src/test/java/info/changelogs/app/repository/ChangeLogRepositoryUnitTest.java
+++ src/test/java/info/changelogs/app/repository/ChangeLogRepositoryUnitTest.java
@@ -3,9 +3,16 @@ package info.changelogs.app.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import info.changelogs.app.entity.ChangeLog;
 
 @DataJpaTest
 class ChangeLogRepositoryUnitTest {
@@ -21,7 +28,14 @@ class ChangeLogRepositoryUnitTest {
 
 	@Test
 	void testFindAllDetailed() {
-		fail("Not yet implemented");
+		Page<ChangeLog> page = changeLogRepository.findAllDetailed(PageRequest.of(0, 1_000));
+		assertThat(page.getTotalElements()).isEqualTo(100);
+		assertThat(page.getContent().parallelStream()
+				.map(ChangeLog::getId).collect(Collectors.toSet()).size())
+			.isEqualTo(100);
+		assertThat(page.getContent().stream()
+				.anyMatch(cl -> cl.getContents() != null || !cl.getContents().isEmpty()))
+			.isTrue();
 	}
 
 	@Test
