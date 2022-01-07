4110cc01645b15e8242ab22c8b708ad6e5539dd4
GLinBoy <hojjatabedie@gmail.com>
2022-01-07 12:58:28 +0330

    📦 NEW: Add ProjectRepository.findOneByTitle unit test
    resolved #94

--- src/test/java/info/changelogs/app/repository/ProjectRepositoryUnitTest.java
+++ src/test/java/info/changelogs/app/repository/ProjectRepositoryUnitTest.java
@@ -3,9 +3,13 @@ package info.changelogs.app.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import info.changelogs.app.entity.Project;
 
 @DataJpaTest
 class ProjectRepositoryUnitTest {
@@ -25,7 +29,8 @@ class ProjectRepositoryUnitTest {
 
 	@Test
 	void testFindOneByTitle() {
-		fail("Not yet implemented");
+		Optional<Project> projectOpt = projectRepository.findOneByTitle("Flexidy");
+		assertThat(projectOpt).isNotEmpty();
 	}
 
 }
