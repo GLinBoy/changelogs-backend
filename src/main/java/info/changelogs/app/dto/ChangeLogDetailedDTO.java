7eb68852ca615b4d85f7014017d6f2fcdf5412d3
GLinBoy <hojjatabedie@gmail.com>
2022-01-08 17:00:47 +0330

    🐛 FIX: Added Serialize\Deserialize config
    #114

--- src/main/java/info/changelogs/app/dto/ChangeLogDetailedDTO.java
+++ src/main/java/info/changelogs/app/dto/ChangeLogDetailedDTO.java
@@ -3,6 +3,11 @@ package info.changelogs.app.dto;
 import java.time.Instant;
 import java.util.Set;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
+
 import info.changelogs.app.entity.BaseEntity;
 import info.changelogs.app.entity.Platform;
 import lombok.AllArgsConstructor;
@@ -12,17 +17,19 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldNameConstants
 @EqualsAndHashCode(callSuper = true)
-public class ChangeLogDetailedDTO extends BaseEntity{
+public class ChangeLogDetailedDTO extends BaseEntity {
 
 	private String versionNo;
 	private String buildVersion;
+
+	@JsonDeserialize(using = InstantDeserializer.class)
+	@JsonSerialize(using = InstantSerializer.class)
 	private Instant releaseDate;
 	private String publisher;
 	private String contact;
