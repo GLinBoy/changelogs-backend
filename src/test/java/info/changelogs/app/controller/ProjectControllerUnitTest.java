51d1eda32bbc198829d4a8c0f2d71d34588b1c3e
GLinBoy <hojjatabedie@gmail.com>
2022-01-09 21:45:13 +0330

    📦 NEW: Added context loaded unit test to ProjectControllerUnitTest

--- src/test/java/info/changelogs/app/controller/ProjectControllerUnitTest.java
+++ src/test/java/info/changelogs/app/controller/ProjectControllerUnitTest.java
@@ -1,10 +1,29 @@
 package info.changelogs.app.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import info.changelogs.app.service.ProjectServiceApi;
+
+@ExtendWith(MockitoExtension.class)
 class ProjectControllerUnitTest {
+	
+	@InjectMocks
+	private ProjectController projectController;
+	
+	@Mock
+	private ProjectServiceApi projectService;
+
+	@Test
+	void contextLoads() {
+		assertThat(projectController).isNotNull();
+	}
 
 	@Test
 	void testGetUserProjectList() {
