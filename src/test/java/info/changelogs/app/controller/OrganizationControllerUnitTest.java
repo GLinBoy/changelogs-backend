afed43075673e77614210ef753be8f9821863416
GLinBoy <hojjatabedie@gmail.com>
2022-01-09 21:43:00 +0330

    🐛 FIX: Added @ExtendWith to OrganizationControllerUnitTest

--- src/test/java/info/changelogs/app/controller/OrganizationControllerUnitTest.java
+++ src/test/java/info/changelogs/app/controller/OrganizationControllerUnitTest.java
@@ -4,11 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import info.changelogs.app.service.OrganizationServiceApi;
 
+@ExtendWith(MockitoExtension.class)
 class OrganizationControllerUnitTest {
 
 	@InjectMocks
