450673d02ec5a43eac5b86658b128934ed2e8bf3
GLinBoy <hojjatabedie@gmail.com>
2022-01-11 14:44:41 +0330

    👌 IMPROVE: Added Mokito anotaions

--- src/test/java/info/changelogs/app/service/impl/GenericServiceImplUnitTest.java
+++ src/test/java/info/changelogs/app/service/impl/GenericServiceImplUnitTest.java
@@ -1,9 +1,30 @@
 package info.changelogs.app.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.doAnswer;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.modelmapper.ModelMapper;
 
+import info.changelogs.app.dto.OrganizationDTO;
+import info.changelogs.app.entity.Organization;
+import info.changelogs.app.repository.OrganizationRepository;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class GenericServiceImplUnitTest {
 
 	@Test
