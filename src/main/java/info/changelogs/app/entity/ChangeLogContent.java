44431be5c2624239a4732cdb0b10d539777eba6b
GLinBoy <hojjatabedie@gmail.com>
2022-01-14 20:13:53 +0330

    👌 IMPROVE: Updated Builder on ChangeLogContent
    resolved #129

--- src/main/java/info/changelogs/app/entity/ChangeLogContent.java
+++ src/main/java/info/changelogs/app/entity/ChangeLogContent.java
@@ -1,5 +1,7 @@
 package info.changelogs.app.entity;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,29 +10,37 @@ import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 
 @Entity
 @Data
-@Builder
 @FieldNameConstants
-@AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true, exclude = "changeLog")
 public class ChangeLogContent extends Auditable {
-	
+
+	ChangeLogContent() {
+		super();
+	}
+
+	@Builder
+	ChangeLogContent(Long id, Boolean isActive, String createdBy, String editedBy, LocalDateTime createdOn,
+			LocalDateTime editedOn, Integer version, ContentType contentType, String content, ChangeLog changeLog) {
+		super(id, isActive, createdBy, editedBy, createdOn, editedOn, version);
+		this.contentType = contentType;
+		this.content = content;
+		this.changeLog = changeLog;
+	}
+
 	@Enumerated(EnumType.STRING)
 	@Column(length = 10, nullable = false)
 	private ContentType contentType;
-	
+
 	@Column(columnDefinition = "TEXT", nullable = false)
 	private String content;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "CHANGELOG_ID", nullable = false, foreignKey = @ForeignKey(name = "FK_CHANGELOG_CONTENT"))
 	private ChangeLog changeLog;
