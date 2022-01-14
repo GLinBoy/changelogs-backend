17f0d9bd44b88e2e2fed37d340733a084bdc54e0
GLinBoy <hojjatabedie@gmail.com>
2022-01-14 20:23:47 +0330

    👌 IMPROVE: Updated Builder on OrganizationMeta
    resolved #125

--- src/main/java/info/changelogs/app/entity/OrganizationMeta.java
+++ src/main/java/info/changelogs/app/entity/OrganizationMeta.java
@@ -7,25 +7,27 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.FieldNameConstants;
 
 @Entity
 @Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@FieldNameConstants
 @EqualsAndHashCode(callSuper = true)
 @Table(uniqueConstraints = {
-		@UniqueConstraint(name = "UNQ_ORGANIZATION_META", columnNames ={"KEY", "ORGANIZATION_ID"})
-})
+		@UniqueConstraint(name = "UNQ_ORGANIZATION_META", columnNames = { "KEY", "ORGANIZATION_ID" }) })
 public class OrganizationMeta extends MetaData {
-	
+
+	public OrganizationMeta() {
+		super();
+	}
+
+	@Builder
+	public OrganizationMeta(Long id, Boolean isActive, String key, String value, Organization organization) {
+		super(id, isActive, key, value);
+		this.organization = organization;
+	}
+
 	@ManyToOne
 	@JoinColumn(name = "ORGANIZATION_ID", nullable = false, foreignKey = @ForeignKey(name = "FK_ORGANIZATION_META"))
 	private Organization organization;
