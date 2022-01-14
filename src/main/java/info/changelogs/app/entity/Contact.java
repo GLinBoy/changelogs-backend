d40609ab8effd4dd83075f1b6d9bdad5edc9b016
GLinBoy <hojjatabedie@gmail.com>
2022-01-14 20:19:50 +0330

    👌 IMPROVE: Updated Builder on Contact resolved #126

--- src/main/java/info/changelogs/app/entity/Contact.java
+++ src/main/java/info/changelogs/app/entity/Contact.java
@@ -6,34 +6,42 @@ import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.FieldNameConstants;
 
 @Entity
 @Data
-@Builder
-@FieldNameConstants
-@AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Contact extends BaseEntity {
-	
+
+	public Contact() {
+		super();
+	}
+
+	@Builder
+	public Contact(Long id, Boolean isActive, String name, String email, String subject, String message,
+			Project project) {
+		super(id, isActive);
+		this.name = name;
+		this.email = email;
+		this.subject = subject;
+		this.message = message;
+		this.project = project;
+	}
+
 	@Column(length = 128, nullable = false)
 	private String name;
-	
+
 	@Column(length = 128, nullable = false)
 	private String email;
-	
+
 	@Column(length = 128, nullable = false)
 	private String subject;
-	
+
 	@Column(columnDefinition = "text", nullable = false)
 	private String message;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "PROJECT_ID", nullable = false, foreignKey = @ForeignKey(name = "FK_PROJECT_CONTACT"))
 	private Project project;
