ad642495a620cd666e2822bf517f740d89d63695
GLinBoy <hojjatabedie@gmail.com>
2022-01-27 20:48:21 +0330

    📦 NEW: Added default username

--- src/test/java/info/changelogs/app/service/impl/ProjectServiceImplUnitTest.java
+++ src/test/java/info/changelogs/app/service/impl/ProjectServiceImplUnitTest.java
@@ -2,6 +2,11 @@ package info.changelogs.app.service.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +17,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 
+import info.changelogs.app.dto.projection.ProjectMinimizedDTO;
 import info.changelogs.app.repository.ProjectRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,6 +39,8 @@ class ProjectServiceImplUnitTest {
 
 	@Mock
 	private ModelMapper modelMapper;
+
+	private final String DEFAULT_USERNAME = "admin";
 
 	@BeforeEach
 	void setUp() {
