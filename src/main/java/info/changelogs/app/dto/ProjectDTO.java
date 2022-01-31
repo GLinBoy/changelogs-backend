069d0e325c4bf69464d6de1c4049c6d68b664a2c
GLinBoy <hojjatabedie@gmail.com>
2022-01-31 10:17:40 +0330

    👌 IMPROVE: Overrided Builder of ProjectDTO
    resolved #148

--- src/main/java/info/changelogs/app/dto/ProjectDTO.java
+++ src/main/java/info/changelogs/app/dto/ProjectDTO.java
@@ -5,54 +5,72 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.URL;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.FieldNameConstants;
 
 @Data
-@Builder
-@FieldNameConstants
-@AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class ProjectDTO extends BaseDTO{
-	
+public class ProjectDTO extends BaseDTO {
+
+	public ProjectDTO() {
+		super();
+	}
+
+	@Builder
+	public ProjectDTO(Long id, Boolean isActive,
+			@Size(min = 4, max = 128) @NotBlank(message = "Name is mandatory") String name,
+			@Size(min = 4, max = 128) @NotBlank(message = "Title is mandatory") String title, String about, String logo,
+			@Size(max = 128) @NotBlank(message = "Owner is mandatory") String owner, Boolean publicAccess,
+			@URL @Size(max = 128) String website, @URL @Size(max = 128) String readmeLink,
+			@Size(max = 32) String license, @URL @Size(max = 128) String licenseLink, Long organizationId) {
+		super(id, isActive);
+		this.name = name;
+		this.title = title;
+		this.about = about;
+		this.logo = logo;
+		this.owner = owner;
+		this.publicAccess = publicAccess;
+		this.website = website;
+		this.readmeLink = readmeLink;
+		this.license = license;
+		this.licenseLink = licenseLink;
+		this.organizationId = organizationId;
+	}
+
 	@Size(min = 4, max = 128)
 	@NotBlank(message = "Name is mandatory")
 	private String name;
-	
+
 	@Size(min = 4, max = 128)
 	@NotBlank(message = "Title is mandatory")
 	private String title;
-	
+
 	private String about;
-	
+
 	private String logo;
-	
+
 	@Size(max = 128)
 	@NotBlank(message = "Owner is mandatory")
 	private String owner;
-	
+
 	private Boolean publicAccess;
-	
+
 	@URL
 	@Size(max = 128)
 	private String website;
-	
+
 	@URL
 	@Size(max = 128)
 	private String readmeLink;
-	
+
 	@Size(max = 32)
 	private String license;
-	
+
 	@URL
 	@Size(max = 128)
 	private String licenseLink;
-	
+
 	private Long organizationId;
 
 }
