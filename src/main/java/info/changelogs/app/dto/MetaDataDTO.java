f1ff6dddff80c282f6e2d33bf36ac7b0c1ef3f05
GLinBoy <hojjatabedie@gmail.com>
2022-01-31 10:13:53 +0330

    👌 IMPROVE: Overrided Builder of MetaDataDTO
    resolved #144

--- src/main/java/info/changelogs/app/dto/MetaDataDTO.java
+++ src/main/java/info/changelogs/app/dto/MetaDataDTO.java
@@ -7,6 +7,16 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public abstract class MetaDataDTO extends BaseDTO {
 
+	MetaDataDTO() {
+		super();
+	}
+
+	MetaDataDTO(Long id, Boolean isActive, String key, String value) {
+		super(id, isActive);
+		this.key = key;
+		this.value = value;
+	}
+
 	private String key;
 	private String value;
 
