982e21a93c2809eecb7854322c0df7be20065825
GLinBoy <hojjatabedie@gmail.com>
2022-01-31 10:15:20 +0330

    👌 IMPROVE: Overrided Builder of ChangeLogDetailedDTO
    resolved #140

--- src/main/java/info/changelogs/app/dto/ChangeLogDetailedDTO.java
+++ src/main/java/info/changelogs/app/dto/ChangeLogDetailedDTO.java
@@ -9,20 +9,33 @@ import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
 
 import info.changelogs.app.entity.Platform;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.FieldNameConstants;
 
 @Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@FieldNameConstants
 @EqualsAndHashCode(callSuper = true)
 public class ChangeLogDetailedDTO extends BaseDTO {
+
+	public ChangeLogDetailedDTO() {
+		super();
+	}
+
+	@Builder
+	public ChangeLogDetailedDTO(Long id, Boolean isActive, String versionNo, String buildVersion, Instant releaseDate,
+			String publisher, String contact, Boolean forceUpdate, Platform platform, ProjectDetailedDTO project,
+			Set<ChangeLogContentDTO> contents) {
+		super(id, isActive);
+		this.versionNo = versionNo;
+		this.buildVersion = buildVersion;
+		this.releaseDate = releaseDate;
+		this.publisher = publisher;
+		this.contact = contact;
+		this.forceUpdate = forceUpdate;
+		this.platform = platform;
+		this.project = project;
+		this.contents = contents;
+	}
 
 	private String versionNo;
 	private String buildVersion;
