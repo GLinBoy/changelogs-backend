19c3eb856b24e97f917ca5419d86ec84c0baa4bb
GLinBoy <hojjatabedie@gmail.com>
2022-01-31 10:16:08 +0330

    👌 IMPROVE: Overrided Builder of ContactDTO
    resolved #143

--- src/main/java/info/changelogs/app/dto/ContactDTO.java
+++ src/main/java/info/changelogs/app/dto/ContactDTO.java
@@ -1,19 +1,27 @@
 package info.changelogs.app.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.FieldNameConstants;
 
 @Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@FieldNameConstants
 @EqualsAndHashCode(callSuper = true)
 public class ContactDTO extends BaseDTO {
+
+	public ContactDTO() {
+		super();
+	}
+
+	@Builder
+	public ContactDTO(Long id, Boolean isActive, String name, String email, String subject, String message,
+			Long projectId) {
+		super(id, isActive);
+		this.name = name;
+		this.email = email;
+		this.subject = subject;
+		this.message = message;
+		this.projectId = projectId;
+	}
 
 	private String name;
 	private String email;
