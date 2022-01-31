2e6f85a53382f96fc4063f7b70fd11b8bf5ff62f
GLinBoy <hojjatabedie@gmail.com>
2022-01-31 10:14:27 +0330

    👌 IMPROVE: Overrided Builder of ChangeLogDTO
    resolved #141

--- src/main/java/info/changelogs/app/dto/ChangeLogDTO.java
+++ src/main/java/info/changelogs/app/dto/ChangeLogDTO.java
@@ -14,20 +14,37 @@ import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
 
 import info.changelogs.app.entity.Platform;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.FieldNameConstants;
 
 @Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@FieldNameConstants
 @EqualsAndHashCode(callSuper = true)
 public class ChangeLogDTO extends BaseDTO {
+
+	public ChangeLogDTO() {
+		super();
+	}
+
+	@Builder
+	public ChangeLogDTO(Long id, Boolean isActive, @Size(max = 32) @NotBlank(message = "Version number is mandatory") String versionNo,
+			@Size(max = 32) String buildVersion, @NotNull(message = "Release date is mandatory") Instant releaseDate,
+			@Size(max = 128) @NotBlank(message = "Publisher is mandatory") String publisher,
+			@Email(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$") @Size(min = 8, max = 64) @NotBlank(message = "Contact is mandatory") String contact,
+			@NotNull(message = "Force update is mandatory") Boolean forceUpdate,
+			@NotNull(message = "Platform is mandatory") Platform platform,
+			@NotNull(message = "Project is mandatory") Long projectId, Set<ChangeLogContentDTO> contents) {
+		super(id, isActive);
+		this.versionNo = versionNo;
+		this.buildVersion = buildVersion;
+		this.releaseDate = releaseDate;
+		this.publisher = publisher;
+		this.contact = contact;
+		this.forceUpdate = forceUpdate;
+		this.platform = platform;
+		this.projectId = projectId;
+		this.contents = contents;
+	}
 
 	@Size(max = 32)
 	@NotBlank(message = "Version number is mandatory")
