0c9ef77c1721720885c45da605f957833d9e4cc4
GLinBoy <hojjatabedie@gmail.com>
2022-02-01 11:22:24 +0330

    👌 IMPROVE: Enabled test to use Mokito

--- src/test/java/info/changelogs/app/controller/GenericControllerUnitTest.java
+++ src/test/java/info/changelogs/app/controller/GenericControllerUnitTest.java
@@ -1,9 +1,34 @@
 package info.changelogs.app.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.doReturn;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 
+import info.changelogs.app.dto.OrganizationDTO;
+import info.changelogs.app.service.OrganizationServiceApi;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class GenericControllerUnitTest {
 
 	@Test
