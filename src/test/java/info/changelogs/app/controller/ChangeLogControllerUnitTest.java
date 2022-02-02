e133b80b128e3333a4788f2fcd3d6a56620291d1
GLinBoy <hojjatabedie@gmail.com>
2022-02-02 19:00:30 +0330

    📦 NEW: Added default values for test

--- src/test/java/info/changelogs/app/controller/ChangeLogControllerUnitTest.java
+++ src/test/java/info/changelogs/app/controller/ChangeLogControllerUnitTest.java
@@ -1,14 +1,35 @@
 package info.changelogs.app.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.doReturn;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 
+import info.changelogs.app.dto.ChangeLogContentDTO;
+import info.changelogs.app.dto.ChangeLogDTO;
+import info.changelogs.app.dto.ChangeLogDetailedDTO;
+import info.changelogs.app.entity.ContentType;
+import info.changelogs.app.entity.Platform;
 import info.changelogs.app.service.ChangeLogServiceApi;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,6 +40,11 @@ class ChangeLogControllerUnitTest {
 
 	@Mock
 	private ChangeLogServiceApi changeLogService;
+
+	private final Integer DEFAULT_CHANGELOG_COUNT = 3;
+	private final String DEFAULT_USERNAME = "admin";
+	private final String DEFAULT_TITLE = "title";
+	private final String DEFAULT_VERSION = "1.2.3";
 
 	@Test
 	void contextLoads() {
