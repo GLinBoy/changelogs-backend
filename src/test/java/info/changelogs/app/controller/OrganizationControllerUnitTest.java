ffc1c6afad7118733de63b94540a2c5e783f7533
GLinBoy <hojjatabedie@gmail.com>
2022-02-04 14:50:51 +0330

    📦 NEW: Added a method to generate owner

--- src/test/java/info/changelogs/app/controller/OrganizationControllerUnitTest.java
+++ src/test/java/info/changelogs/app/controller/OrganizationControllerUnitTest.java
@@ -41,6 +41,16 @@ class OrganizationControllerUnitTest {
 	@Test
 	void testGetOwnerList() {
 		fail("Not yet implemented");
+
+	private List<OwnerDTO> generateOwnerDTO(Integer count) {
+		return IntStream.range(0, count).mapToObj(i -> {
+			ProjectionFactory factory = new SpelAwareProxyProjectionFactory();
+			OwnerDTO owner = factory.createProjection(OwnerDTO.class);
+			owner.setId(Long.valueOf(1001 + i));
+			owner.setName(String.format("%s %d", "name", i));
+			owner.setTitle(String.format("%s %d", "title", i));
+			return owner;
+		}).collect(Collectors.toList());
 	}
 
 }
