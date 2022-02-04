71bc63ad3b55d6697d8d64902281b5b86cd67be9
GLinBoy <hojjatabedie@gmail.com>
2022-02-04 15:49:50 +0330

    📦 NEW: Added default variable and pageable

--- src/test/java/info/changelogs/app/controller/ProjectControllerUnitTest.java
+++ src/test/java/info/changelogs/app/controller/ProjectControllerUnitTest.java
@@ -1,14 +1,31 @@
 package info.changelogs.app.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 
+import info.changelogs.app.dto.projection.ProjectMinimizedDTO;
 import info.changelogs.app.service.ProjectServiceApi;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,6 +36,10 @@ class ProjectControllerUnitTest {
 	
 	@Mock
 	private ProjectServiceApi projectService;
+
+
+	private final Integer DEFAULT_SIZE = 3;
+	private final Pageable pageable = PageRequest.of(0, 1_000);
 
 	@Test
 	void contextLoads() {
