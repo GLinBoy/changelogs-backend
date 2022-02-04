ae886fb5435daf2e2d97d0cc22c7f38afab0e289
GLinBoy <hojjatabedie@gmail.com>
2022-02-04 14:38:57 +0330

    📦 NEW: Added MockHttpServletResponse

--- src/test/java/info/changelogs/app/controller/HomeControllerUnitTest.java
+++ src/test/java/info/changelogs/app/controller/HomeControllerUnitTest.java
@@ -2,18 +2,30 @@ package info.changelogs.app.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import java.io.IOException;
+
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 @ExtendWith(MockitoExtension.class)
 class HomeControllerUnitTest {
 
 	@InjectMocks
 	private HomeController homeController;
-	
+
+	@Mock
+	private MockHttpServletResponse response;
+
 	@Test
 	void contextLoads() {
 		assertThat(homeController).isNotNull();
