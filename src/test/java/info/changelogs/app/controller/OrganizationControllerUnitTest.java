ba8cf1139f56e783e739617fe6018b4ca6ed4772
GLinBoy <hojjatabedie@gmail.com>
2022-02-04 14:50:20 +0330

    📦 NEW: Added default size of owner

--- src/test/java/info/changelogs/app/controller/OrganizationControllerUnitTest.java
+++ src/test/java/info/changelogs/app/controller/OrganizationControllerUnitTest.java
@@ -1,14 +1,25 @@
 package info.changelogs.app.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import info.changelogs.app.dto.projection.OwnerDTO;
 import info.changelogs.app.service.OrganizationServiceApi;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,6 +30,8 @@ class OrganizationControllerUnitTest {
 
 	@Mock
 	private OrganizationServiceApi organizationService;
+
+	private final Integer DEFAULT_SIZE = 3;
 
 	@Test
 	void contextLoads() {
