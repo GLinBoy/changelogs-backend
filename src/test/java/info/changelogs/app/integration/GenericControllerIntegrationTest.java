7bda555d7b12cb140073fb66faeafd44d9056ff6
GLinBoy <hojjatabedie@gmail.com>
2022-02-05 14:12:27 +0330

    📦 NEW: Added integration test for GenericController
    #108

---
+++ src/test/java/info/changelogs/app/integration/GenericControllerIntegrationTest.java
@@ -0,0 +1,39 @@
+package info.changelogs.app.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GenericControllerIntegrationTest {
+
+	@Test
+	void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGenericController() {
+		fail("Not yet implemented");
+	}
+
+}
