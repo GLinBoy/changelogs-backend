355c500fc12368e9cad863fc987ea1399e066192
GLinBoy <hojjatabedie@gmail.com>
2022-02-06 16:55:57 +0330

    📦 NEW: Added integration test file for OrganizationController
    #111

---
+++ src/test/java/info/changelogs/app/integration/OrganizationControllerIntegrationTest.java
@@ -0,0 +1,19 @@
+package info.changelogs.app.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrganizationControllerIntegrationTest {
+
+	@Test
+	void testOrganizationController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetOwnerList() {
+		fail("Not yet implemented");
+	}
+
+}
