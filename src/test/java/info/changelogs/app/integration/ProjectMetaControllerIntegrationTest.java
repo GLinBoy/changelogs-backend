962dcc61d125d839b79e7f044ccdc744ca781f00
GLinBoy <hojjatabedie@gmail.com>
2022-02-06 16:57:54 +0330

    📦 NEW: Added integration test file for ProjectMetaController
    #154

---
+++ src/test/java/info/changelogs/app/integration/ProjectMetaControllerIntegrationTest.java
@@ -0,0 +1,14 @@
+package info.changelogs.app.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectMetaControllerIntegrationTest {
+
+	@Test
+	void testProjectMetaController() {
+		fail("Not yet implemented");
+	}
+
+}
