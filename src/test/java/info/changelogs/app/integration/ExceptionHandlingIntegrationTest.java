8181e0b4e0d1d71f3df37c83300ec89ec1845e84
GLinBoy <hojjatabedie@gmail.com>
2022-02-06 16:59:16 +0330

    📦 NEW: Added integration test file for ExceptionHandling
    #156

---
+++ src/test/java/info/changelogs/app/integration/ExceptionHandlingIntegrationTest.java
@@ -0,0 +1,14 @@
+package info.changelogs.app.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ExceptionHandlingIntegrationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
