a05245da082a5f05a8970a0cbbcfe6fd5d113776
GLinBoy <hojjatabedie@gmail.com>
2022-02-06 16:54:59 +0330

    📦 NEW: Added integration test file for ChangeLogMetaController
    #152

---
+++ src/test/java/info/changelogs/app/integration/ChangeLogMetaControllerIntegrationTest.java
@@ -0,0 +1,14 @@
+package info.changelogs.app.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChangeLogMetaControllerIntegrationTest {
+
+	@Test
+	void testChangeLogMetaController() {
+		fail("Not yet implemented");
+	}
+
+}
