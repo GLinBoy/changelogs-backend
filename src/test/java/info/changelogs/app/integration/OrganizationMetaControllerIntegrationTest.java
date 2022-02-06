5f74a6ae2948aed9a14c4e976f07d2a860bfdf8a
GLinBoy <hojjatabedie@gmail.com>
2022-02-06 16:57:07 +0330

    📦 NEW: Added integration test file for OrganizationMetaController
    #155

---
+++ src/test/java/info/changelogs/app/integration/OrganizationMetaControllerIntegrationTest.java
@@ -0,0 +1,14 @@
+package info.changelogs.app.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrganizationMetaControllerIntegrationTest {
+
+	@Test
+	void testOrganizationMetaController() {
+		fail("Not yet implemented");
+	}
+
+}
