25959965c7d51473912645b74f62097b03343fd6
GLinBoy <hojjatabedie@gmail.com>
2022-02-06 16:53:54 +0330

    📦 NEW: Added integration test file for ChangeLogContent
    #151

---
+++ src/test/java/info/changelogs/app/integration/ChangeLogContentControllerIntegrationTest.java
@@ -0,0 +1,14 @@
+package info.changelogs.app.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChangeLogContentControllerIntegrationTest {
+
+	@Test
+	void testChangeLogContentController() {
+		fail("Not yet implemented");
+	}
+
+}
