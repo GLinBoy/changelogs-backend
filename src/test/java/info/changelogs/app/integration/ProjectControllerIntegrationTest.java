add2402b3b3d5bbe69f155902dc4dcfcda4755fa
GLinBoy <hojjatabedie@gmail.com>
2022-02-06 16:57:31 +0330

    📦 NEW: Added integration test file for ProjectController
    #112

---
+++ src/test/java/info/changelogs/app/integration/ProjectControllerIntegrationTest.java
@@ -0,0 +1,24 @@
+package info.changelogs.app.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectControllerIntegrationTest {
+
+	@Test
+	void testProjectController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserProjectList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProjectDetailByTitle() {
+		fail("Not yet implemented");
+	}
+
+}
