8b3710a4b040403a2392130b8b90e90e5b2c7bc8
GLinBoy <hojjatabedie@gmail.com>
2022-03-20 17:11:25 +0330

    👌 IMPROVE: Extended from AuditableDTO

--- src/main/java/info/changelogs/app/dto/ProjectDTO.java
+++ src/main/java/info/changelogs/app/dto/ProjectDTO.java
@@ -1,5 +1,7 @@
 package info.changelogs.app.dto;
 
+import java.time.LocalDateTime;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -11,20 +13,21 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class ProjectDTO extends BaseDTO {
+public class ProjectDTO extends AuditableDTO {
 
 	public ProjectDTO() {
 		super();
 	}
 
 	@Builder
-	public ProjectDTO(Long id, Boolean isActive,
+	public ProjectDTO(Long id, Boolean isActive, String createdBy, String editedBy, LocalDateTime createdOn,
+			LocalDateTime editedOn, Integer version,
 			@Size(min = 4, max = 128) @NotBlank(message = "Name is mandatory") String name,
 			@Size(min = 4, max = 128) @NotBlank(message = "Title is mandatory") String title, String about, String logo,
 			@Size(max = 128) @NotBlank(message = "Owner is mandatory") String owner, Boolean publicAccess,
 			@URL @Size(max = 128) String website, @URL @Size(max = 128) String readmeLink,
 			@Size(max = 32) String license, @URL @Size(max = 128) String licenseLink, Long organizationId) {
-		super(id, isActive);
+		super(id, isActive, createdBy, editedBy, createdOn, editedOn, version);
 		this.name = name;
 		this.title = title;
 		this.about = about;
