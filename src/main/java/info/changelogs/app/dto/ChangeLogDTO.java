35c838b8f3ab1d92e00d98abad4109d460320fe6
GLinBoy <hojjatabedie@gmail.com>
2022-03-20 17:10:20 +0330

    👌 IMPROVE: Extended from AuditableDTO

--- src/main/java/info/changelogs/app/dto/ChangeLogDTO.java
+++ src/main/java/info/changelogs/app/dto/ChangeLogDTO.java
@@ -1,6 +1,7 @@
 package info.changelogs.app.dto;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import javax.validation.constraints.Email;
@@ -20,21 +21,24 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class ChangeLogDTO extends BaseDTO {
+public class ChangeLogDTO extends AuditableDTO {
 
 	public ChangeLogDTO() {
 		super();
 	}
 
 	@Builder
-	public ChangeLogDTO(Long id, Boolean isActive, @Size(max = 32) @NotBlank(message = "Version number is mandatory") String versionNo,
+	public ChangeLogDTO(Long id, Boolean isActive, String createdBy, String editedBy, LocalDateTime createdOn,
+			LocalDateTime editedOn, Integer version,
+			@Size(max = 32) @NotBlank(message = "Version number is mandatory") String versionNo,
 			@Size(max = 32) String buildVersion, @NotNull(message = "Release date is mandatory") Instant releaseDate,
 			@Size(max = 128) @NotBlank(message = "Publisher is mandatory") String publisher,
-			@Email(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$") @Size(min = 8, max = 64) @NotBlank(message = "Contact is mandatory") String contact,
+			@Email(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$")
+			@Size(min = 8, max = 64) @NotBlank(message = "Contact is mandatory") String contact,
 			@NotNull(message = "Force update is mandatory") Boolean forceUpdate,
 			@NotNull(message = "Platform is mandatory") Platform platform,
 			@NotNull(message = "Project is mandatory") Long projectId, Set<ChangeLogContentDTO> contents) {
-		super(id, isActive);
+		super(id, isActive, createdBy, editedBy, createdOn, editedOn, version);
 		this.versionNo = versionNo;
 		this.buildVersion = buildVersion;
 		this.releaseDate = releaseDate;
