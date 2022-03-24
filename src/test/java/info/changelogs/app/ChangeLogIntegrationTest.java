ef4c9063e031c0d3ead64e01d7453a2d8ef9be29
GLinBoy <hojjatabedie@gmail.com>
2022-03-24 15:17:37 +0430

    👌 IMPROVE: Removed random port

--- src/test/java/info/changelogs/app/ChangeLogIntegrationTest.java
+++ src/test/java/info/changelogs/app/ChangeLogIntegrationTest.java
@@ -5,15 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.test.context.ActiveProfiles;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @ActiveProfiles
+@AutoConfigureMockMvc
 public @interface ChangeLogIntegrationTest {
   @AliasFor(annotation = ActiveProfiles.class, attribute = "profiles") String[] activeProfiles() default {"test"};
 }
