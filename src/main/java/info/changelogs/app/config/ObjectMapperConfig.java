cbbe8f71f4450405184cb9a992c8a124ad3c2e80
GLinBoy <hojjatabedie@gmail.com>
2022-04-23 17:01:48 +0200

    📦 NEW: Created a class to config ObjectMapper

---
+++ src/main/java/info/changelogs/app/config/ObjectMapperConfig.java
@@ -0,0 +1,25 @@
+package info.changelogs.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.zalando.problem.jackson.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class ObjectMapperConfig {
+
+	@Bean
+	@Primary
+	public ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper = mapper.findAndRegisterModules();
+		
+		// Registered Problem Module
+		mapper.registerModules(new ProblemModule(),
+				new ConstraintViolationProblemModule());
+		return mapper;
+	}
+}
