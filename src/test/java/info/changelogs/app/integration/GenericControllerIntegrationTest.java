ae49a4a45693fb1eb5cebd64f4004cc3f0bf669d
GLinBoy <hojjatabedie@gmail.com>
2022-04-30 14:33:33 +0200

    🐛 FIX: ObjectMapper to handle java.time.Instant

--- src/test/java/info/changelogs/app/integration/GenericControllerIntegrationTest.java
+++ src/test/java/info/changelogs/app/integration/GenericControllerIntegrationTest.java
@@ -18,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.zalando.problem.jackson.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -84,11 +86,16 @@ class GenericControllerIntegrationTest {
 	}
 	
 	public static String asJsonString(final Object obj) {
-	    try {
-	        return new ObjectMapper().writeValueAsString(obj);
-	    } catch (Exception e) {
-	        throw new RuntimeException(e);
-	    }
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			mapper = mapper.findAndRegisterModules();
+
+			// Registered Problem Module
+			mapper.registerModules(new ProblemModule(), new ConstraintViolationProblemModule());
+			return mapper.writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Test
