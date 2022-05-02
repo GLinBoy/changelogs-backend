f6ee3d2990eef58d1fb095593cbd50a7fd31d142
GLinBoy <hojjatabedie@gmail.com>
2022-05-02 19:24:00 +0200

    📦 NEW: Added test to check context for ChangeLogMetaController

--- src/test/java/info/changelogs/app/integration/ChangeLogMetaControllerIntegrationTest.java
+++ src/test/java/info/changelogs/app/integration/ChangeLogMetaControllerIntegrationTest.java
@@ -11,7 +11,12 @@ import info.changelogs.app.controller.ChangeLogMetaController;
 @ChangeLogIntegrationTest
 class ChangeLogMetaControllerIntegrationTest {
 
+	@Autowired
+	private ChangeLogMetaController changeLogMetaController;
+
 	@Test
+	void contextLoads() {
+		assertThat(changeLogMetaController).isNotNull();
 	}
 
 }
