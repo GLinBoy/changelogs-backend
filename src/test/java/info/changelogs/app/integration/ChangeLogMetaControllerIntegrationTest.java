e56f9a4e7512d31f05f24f8e69b2b997052d87fe
GLinBoy <hojjatabedie@gmail.com>
2022-05-02 19:22:51 +0200

    👌 IMPROVE: Make ChangeLogMetaControllerIntegrationTest testable

--- src/test/java/info/changelogs/app/integration/ChangeLogMetaControllerIntegrationTest.java
+++ src/test/java/info/changelogs/app/integration/ChangeLogMetaControllerIntegrationTest.java
@@ -1,9 +1,14 @@
 package info.changelogs.app.integration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import info.changelogs.app.ChangeLogIntegrationTest;
+import info.changelogs.app.controller.ChangeLogMetaController;
+
+@ChangeLogIntegrationTest
 class ChangeLogMetaControllerIntegrationTest {
 
 	@Test
