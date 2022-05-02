9efbb09dda3a9047faefac677c8390dbff608b3f
GLinBoy <hojjatabedie@gmail.com>
2022-05-02 19:14:30 +0200

    👌 IMPROVE: Inject ChangeLogController and MockMVC

--- src/test/java/info/changelogs/app/integration/ChangeLogControllerIntegrationTest.java
+++ src/test/java/info/changelogs/app/integration/ChangeLogControllerIntegrationTest.java
@@ -1,13 +1,33 @@
 package info.changelogs.app.integration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.isNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.core.Every;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import info.changelogs.app.ChangeLogIntegrationTest;
+import info.changelogs.app.controller.ChangeLogController;
 
 @ChangeLogIntegrationTest
 class ChangeLogControllerIntegrationTest {
+
+	@Autowired
+	private ChangeLogController changeLogController;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void testChangeLogController() {
