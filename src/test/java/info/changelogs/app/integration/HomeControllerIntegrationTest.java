96d3a760246a2f06553df86b49869b4b7a3cdcb0
GLinBoy <hojjatabedie@gmail.com>
2022-05-03 21:11:57 +0200

    👌 IMPROVE: Made HomeControllerIntegrationTest testable

--- src/test/java/info/changelogs/app/integration/HomeControllerIntegrationTest.java
+++ src/test/java/info/changelogs/app/integration/HomeControllerIntegrationTest.java
@@ -1,9 +1,20 @@
 package info.changelogs.app.integration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.web.servlet.MockMvc;
 
+import info.changelogs.app.ChangeLogIntegrationTest;
+import info.changelogs.app.controller.HomeController;
+
+@ChangeLogIntegrationTest
 class HomeControllerIntegrationTest {
 
 	@Test
