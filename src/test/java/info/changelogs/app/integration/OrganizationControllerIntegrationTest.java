fd2e4ba6011bb5b36c44777e3e697cee7cb6088d
GLinBoy <hojjatabedie@gmail.com>
2022-05-03 21:15:02 +0200

    👌 IMPROVE: Made OrganizationControllerIntegrationTest testable

--- src/test/java/info/changelogs/app/integration/OrganizationControllerIntegrationTest.java
+++ src/test/java/info/changelogs/app/integration/OrganizationControllerIntegrationTest.java
@@ -1,9 +1,25 @@
 package info.changelogs.app.integration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.everyItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import info.changelogs.app.ChangeLogIntegrationTest;
+import info.changelogs.app.controller.OrganizationController;
+
+@ChangeLogIntegrationTest
 class OrganizationControllerIntegrationTest {
 
 	@Test
