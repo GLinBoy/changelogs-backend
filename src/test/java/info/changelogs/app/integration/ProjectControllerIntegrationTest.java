89fe6ca6a93622b95f0e553aa14fc625fd384c88
GLinBoy <hojjatabedie@gmail.com>
2022-05-04 20:44:35 +0200

    👌 IMPROVE: Made ProjectControllerIntegraitionTest testable

--- src/test/java/info/changelogs/app/integration/ProjectControllerIntegrationTest.java
+++ src/test/java/info/changelogs/app/integration/ProjectControllerIntegrationTest.java
@@ -1,9 +1,15 @@
 package info.changelogs.app.integration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import info.changelogs.app.ChangeLogIntegrationTest;
+import info.changelogs.app.controller.ProjectController;
+
+@ChangeLogIntegrationTest
 class ProjectControllerIntegrationTest {
 
 	@Test
