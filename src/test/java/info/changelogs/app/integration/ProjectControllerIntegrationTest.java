9035e3960c72348e90d78c44861cab86c74a4c91
GLinBoy <hojjatabedie@gmail.com>
2022-05-05 21:04:41 +0200

    📦 NEW: Added requirement variables

--- src/test/java/info/changelogs/app/integration/ProjectControllerIntegrationTest.java
+++ src/test/java/info/changelogs/app/integration/ProjectControllerIntegrationTest.java
@@ -1,10 +1,20 @@
 package info.changelogs.app.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.hamcrest.CoreMatchers.everyItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import info.changelogs.app.ChangeLogIntegrationTest;
 import info.changelogs.app.controller.ProjectController;
@@ -14,6 +24,13 @@ class ProjectControllerIntegrationTest {
 
 	@Autowired
 	private ProjectController projectController;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Value("${application.api-path}/project")
+	private String baseUrl;
+	private final String TITLE = "Flexidy";
 
 	@Test
 	void contextLoads() {
