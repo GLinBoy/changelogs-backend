17ac8284ca781c0580129dc6b50b57eae5978ad7
GLinBoy <hojjatabedie@gmail.com>
2022-06-09 21:51:29 +0200

    🐛 FIX: Redirect to swagger ui

--- src/main/java/info/changelogs/app/controller/HomeController.java
+++ src/main/java/info/changelogs/app/controller/HomeController.java
@@ -4,19 +4,15 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
-	@Value("${application.api-path}")
-	private String apiPath;
-	
+
 	@GetMapping(value = "/")
 	public void redirect(HttpServletResponse response) throws IOException {
-		response.sendRedirect(apiPath.concat("/swagger-ui.html"));
+		response.sendRedirect("/swagger-ui.html");
 	}
 
 }
