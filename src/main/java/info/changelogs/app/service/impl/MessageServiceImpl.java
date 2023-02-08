1a1f18c4c434e54c9bccdf4e80105d270d0d123d
GLinBoy <hojjatabedie@gmail.com>
2023-02-08 14:40:16 +0100

    👌 IMPROVE: Renamed ContactServiceImpl to MessageServiceImpl

--- src/main/java/info/changelogs/app/service/impl/MessageServiceImpl.java
+++ src/main/java/info/changelogs/app/service/impl/MessageServiceImpl.java
@@ -3,18 +3,18 @@ package info.changelogs.app.service.impl;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import info.changelogs.app.dto.ContactDTO;
-import info.changelogs.app.entity.Contact;
-import info.changelogs.app.repository.ContactRepository;
-import info.changelogs.app.service.ContactServiceApi;
+import info.changelogs.app.dto.MessageDTO;
+import info.changelogs.app.entity.Message;
+import info.changelogs.app.repository.MessageRepository;
+import info.changelogs.app.service.MessageServiceApi;
 
 @Service
-public class ContactServiceImpl
-	extends GenericServiceImpl<ContactDTO, Contact, ContactRepository>
-	implements ContactServiceApi {
+public class MessageServiceImpl
+	extends GenericServiceImpl<MessageDTO, Message, MessageRepository>
+	implements MessageServiceApi {
 
-	public ContactServiceImpl(ContactRepository repository, ModelMapper mapper) {
-		super(repository, mapper, ContactDTO.class, Contact.class);
+	public MessageServiceImpl(MessageRepository repository, ModelMapper mapper) {
+		super(repository, mapper, MessageDTO.class, Message.class);
 	}
 
 }
