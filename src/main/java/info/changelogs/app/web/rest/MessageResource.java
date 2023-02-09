03ba320cacb56bad968fb0433e43dd3337ca4ade
GLinBoy <hojjatabedie@gmail.com>
2023-02-09 10:48:41 +0100

    👌 IMPROVE: Renamed contact to message

--- src/main/java/info/changelogs/app/web/rest/MessageResource.java
+++ src/main/java/info/changelogs/app/web/rest/MessageResource.java
@@ -3,14 +3,14 @@ package info.changelogs.app.web.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import info.changelogs.app.dto.ContactDTO;
-import info.changelogs.app.service.ContactServiceApi;
+import info.changelogs.app.dto.MessageDTO;
+import info.changelogs.app.service.MessageServiceApi;
 
 @RestController
-@RequestMapping(path = "${application.api-path}/contact")
-public class ContactResource extends GenericResource<ContactDTO, ContactServiceApi> {
+@RequestMapping(path = "${application.api-path}/messages")
+public class MessageResource extends GenericResource<MessageDTO, MessageServiceApi> {
 
-	public ContactResource(ContactServiceApi service) {
+	public MessageResource(MessageServiceApi service) {
 		super(service);
 	}
 
