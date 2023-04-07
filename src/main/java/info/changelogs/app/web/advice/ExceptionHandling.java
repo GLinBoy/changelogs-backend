05ab5d05c12143821885f82a659fdb79877c5457
GLinBoy <hojjatabedie@gmail.com>
2023-04-07 19:46:49 +0200

    👌 IMPROVE: Apply Spring Problem Details
    resolved #255

--- src/main/java/info/changelogs/app/web/advice/ExceptionHandling.java
+++ src/main/java/info/changelogs/app/web/advice/ExceptionHandling.java
@@ -1,8 +1,9 @@
 package info.changelogs.app.web.advice;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-final class ExceptionHandling {
+final class ExceptionHandling extends ResponseEntityExceptionHandler {
 
 }
