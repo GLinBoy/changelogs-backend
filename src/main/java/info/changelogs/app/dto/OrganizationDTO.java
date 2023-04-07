ccf36028aaff6d829e1aba61115d6109c2e5134d
GLinBoy <hojjatabedie@gmail.com>
2023-04-07 19:43:16 +0200

    👌 IMPROVE: Replaced javax.validation.* with jakarta.validation.*

--- src/main/java/info/changelogs/app/dto/OrganizationDTO.java
+++ src/main/java/info/changelogs/app/dto/OrganizationDTO.java
@@ -2,12 +2,11 @@ package info.changelogs.app.dto;
 
 import java.time.Instant;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 import org.hibernate.validator.constraints.URL;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
