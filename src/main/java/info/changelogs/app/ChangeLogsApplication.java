366742d41da029bc914b34b7f0f2e3ea8dd13fb9
GLinBoy <glinboy@live.com>
2025-12-25 13:41:42 +0100

    👌 IMPROVE: Remove unused Spring Boot auto-configuration imports

--- src/main/java/info/changelogs/app/ChangeLogsApplication.java
+++ src/main/java/info/changelogs/app/ChangeLogsApplication.java
@@ -1,9 +1,7 @@
 package info.changelogs.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import info.changelogs.app.util.ApplicationInformation;
@@ -11,7 +9,6 @@ import info.changelogs.app.util.ApplicationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ ApplicationProperties.class, ApplicationInformation.class })
-@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
 public class ChangeLogsApplication {
 
 	public static void main(String[] args) {
